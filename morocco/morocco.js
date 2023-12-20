a0d2c1b46aee4fcfe031194db7038e67e2095f37
Sabrine996 <150662079+Sabrine996@users.noreply.github.com>
2023-12-20 15:13:57 +0100

    final design (hopefully 🤞)
    final update of Morocco

--- morocco/morocco.js
+++ morocco/morocco.js
@@ -3,47 +3,55 @@ $(document).ready(function() {
   $(window).scroll(function () {
     var heroHeight = $("header").height();
     var yPosition = $(document).scrollTop();
-  
-    if (yPosition <= heroHeight) {
-      var effectFactor = yPosition / heroHeight;
-      var rotation =
-        effectFactor *
-        (Math.PI / 2 - Math.asin((heroHeight - yPosition) / heroHeight));
-      $(".hero")
-        .css({
-          "-webkit-transform": "rotateX(" + rotation + "rad)",
-          transform: "rotateX(" + rotation + "rad)"
-        })
-        .find(".overlay")
-        .css("opacity", effectFactor);
+
+    if (heroHeight) { // Checking if heroHeight is not null or undefined
+      if (yPosition <= heroHeight) {
+        var effectFactor = yPosition / heroHeight;
+        var rotation =
+          effectFactor *
+          (Math.PI / 2 - Math.asin((heroHeight - yPosition) / heroHeight));
+        $(".hero")
+          .css({
+            "-webkit-transform": "rotateX(" + rotation + "rad)",
+            transform: "rotateX(" + rotation + "rad)"
+          })
+          .find(".overlay")
+          .css("opacity", effectFactor);
+      }
     }
 
-    if (yPosition <= heroHeight) {
-      $("nav").removeClass("fixed");
-    } else {
-      $("nav").addClass("fixed");
+    var navElement = $("nav");
+    if (navElement.length > 0) { // Checking if navElement exists
+      if (yPosition <= heroHeight) {
+        navElement.removeClass("fixed");
+      } else {
+        navElement.addClass("fixed");
+      }
     }
   });
 
   // Back to top button
   const button = document.getElementById("back-to-top-btn");
 
-  window.addEventListener("scroll", scrollFunction);
+  if (button) { // Checking if button exists
+    window.addEventListener("scroll", scrollFunction);
 
-  function scrollFunction() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-      button.style.display = "block";
-    } else {
-      button.style.display = "none";
+    function scrollFunction() {
+      var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+      if (scrollTop > 300) {
+        button.style.display = "block";
+      } else {
+        button.style.display = "none";
+      }
     }
-  }
 
-  button.addEventListener("click", backToTop);
+    button.addEventListener("click", backToTop);
 
-  function backToTop() {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+    function backToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
   }
 });
